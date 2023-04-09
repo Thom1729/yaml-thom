3651696fbdf1ba72d78506e7ecf1567b6dfe194c
Thomas Smith <thom@thomthesmith.com>
2023-04-09 15:00:42 -0400

    Factor out stdlib

--- src/util/collection.ts
+++ src/util/collection.ts
@@ -1,17 +1,17 @@
-export function single<T>(itr: Iterable<T>) {
+export function single<T>(itr: Iterable<T>, msg?: string) {
   const arr = Array.from(itr);
   switch (arr.length) {
-    case 0: throw new TypeError(`No value`);
+    case 0: throw new TypeError(msg ?? `No value`);
     case 1: return arr[0];
-    default: throw new TypeError(`${arr.length} values`);
+    default: throw new TypeError(msg ?? `${arr.length} values`);
   }
 }
 
-export function singleOrNull<T>(itr: Iterable<T>) {
+export function singleOrNull<T>(itr: Iterable<T>, msg?: string) {
   const arr = Array.from(itr);
   switch (arr.length) {
     case 0: return null;
     case 1: return arr[0];
-    default: throw new TypeError(`${arr.length} values`);
+    default: throw new TypeError(msg ?? `${arr.length} values`);
   }
 }
