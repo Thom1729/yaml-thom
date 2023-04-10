529efb84410cacb99d310b3a09b9732c9098c0c4
Thomas Smith <thom@thomthesmith.com>
2023-04-10 15:46:43 -0400

    Fix assertions

--- src/evaluator/helpers.ts
+++ src/evaluator/helpers.ts
@@ -10,7 +10,7 @@ export function isStr(node: RepresentationNode): node is RepresentationScalar<'t
 }
 
 export function assertStr(node: RepresentationNode, message?: string): asserts node is RepresentationScalar<'tag:yaml.org,2002:str'> {
-  if (node.kind !== 'scalar' && node.tag !== 'tag:yaml.org,2002:str') throw new TypeError(message ?? 'expected str');
+  if (node.kind !== 'scalar' || node.tag !== 'tag:yaml.org,2002:str') throw new TypeError(message ?? 'expected str');
 }
 
 export function isNull(node: RepresentationNode): node is RepresentationScalar<'tag:yaml.org,2002:null'> {
@@ -18,7 +18,7 @@ export function isNull(node: RepresentationNode): node is RepresentationScalar<'
 }
 
 export function assertNull(node: RepresentationNode, message?: string): asserts node is RepresentationScalar<'tag:yaml.org,2002:null'> {
-  if (node.kind !== 'scalar' && node.tag !== 'tag:yaml.org,2002:null') throw new TypeError(message ?? 'expected null');
+  if (node.kind !== 'scalar' || node.tag !== 'tag:yaml.org,2002:null') throw new TypeError(message ?? 'expected null');
 }
 
 export function isSeq(node: RepresentationNode): node is RepresentationSequence<'tag:yaml.org,2002:seq'> {
@@ -26,7 +26,7 @@ export function isSeq(node: RepresentationNode): node is RepresentationSequence<
 }
 
 export function assertSeq(node: RepresentationNode, message?: string): asserts node is RepresentationSequence<'tag:yaml.org,2002:seq'> {
-  if (node.kind !== 'sequence' && node.tag !== 'tag:yaml.org,2002:seq') throw new TypeError(message ?? 'expected seq');
+  if (node.kind !== 'sequence' || node.tag !== 'tag:yaml.org,2002:seq') throw new TypeError(message ?? 'expected seq');
 }
 
 export function isMap(node: RepresentationNode): node is RepresentationMapping<'tag:yaml.org,2002:map'> {
@@ -34,7 +34,7 @@ export function isMap(node: RepresentationNode): node is RepresentationMapping<'
 }
 
 export function assertMap(node: RepresentationNode, message?: string): asserts node is RepresentationMapping<'tag:yaml.org,2002:map'> {
-  if (node.kind !== 'mapping' && node.tag !== 'tag:yaml.org,2002:map') throw new TypeError(message ?? 'expected map');
+  if (node.kind !== 'mapping' || node.tag !== 'tag:yaml.org,2002:map') throw new TypeError(message ?? 'expected map');
 }
 
 export function isAnnotation(node: RepresentationNode): node is RepresentationMapping<'tag:yaml.org,2002:annotation'> {
@@ -87,7 +87,7 @@ export function _extractStringMap<T extends string>(
 }
 
 export function extractAnnotationInfo(annotation: RepresentationMapping) {
-  const { name, value, arguments: args } = extractStringMap(annotation, ['name', 'value', 'arguments']);
+  const { name, value, arguments: args } = _extractStringMap(Array.from(annotation), ['name', 'value', 'arguments']);
 
   return {
     name: extractStrContent(name),
