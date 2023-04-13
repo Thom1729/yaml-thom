25f32ec789db69704bdf581cbdf61e1c2bd41078
Thomas Smith <thom@thomthesmith.com>
2023-04-13 00:31:22 -0400

    More annotation functions

--- src/evaluator/stdlib/nodes.ts
+++ src/evaluator/stdlib/nodes.ts
@@ -2,7 +2,11 @@ import { assertNoArgs, type Library } from './util';
 import {
   str, bool,
   isStr, isNull, isBool, isInt, isFloat, isSeq, isMap, int,
+  extractInt,
+  extractSeqItems
 } from '../helpers';
+import { single } from '@/util';
+import { equals } from '@/nodes/equality';
 
 export default {
   kind(value, args) {
@@ -18,6 +22,36 @@ export default {
   size(value, args) {
     assertNoArgs(args);
     return int(BigInt(value.size));
+  },
+
+  get(value, args, context, evaluate) {
+    const key = evaluate(single(args), context);
+    const v = evaluate(value, context);
+    if (v.kind === 'sequence') {
+      const index = Number(extractInt(key));
+      if (index >= v.size) throw new TypeError(`index out of bounds`);
+      return v.get(index);
+    } else if (v.kind === 'mapping') {
+      const result = v.get(key);
+      if (result === null) throw new TypeError(`key not found`);
+      return result;
+    } else {
+      throw new TypeError(`can't call get on scalar`);
+    }
+  },
+
+  equal(child, args, context, evaluate) {
+    assertNoArgs(args);
+    const nodes = extractSeqItems(evaluate(child, context));
+    if (nodes.length === 0) {
+      return bool(true);
+    } else {
+      const [first, ...rest] = nodes;
+      for (const node of rest) {
+        if (!equals(first, node)) return bool(false);
+      }
+      return bool(true);
+    }
   },
 
   isStr(value, args) {
--- src/evaluator/stdlib/seq.ts
+++ src/evaluator/stdlib/seq.ts
@@ -1,15 +1,7 @@
 import { assertNoArgs, type Library } from './util';
-import { extractInt, assertSeq, extractSeqItems, seq } from '../helpers';
-import { single } from '@/util';
+import { extractSeqItems, seq } from '../helpers';
 
 export default {
-  get(value, args) {
-    assertSeq(value);
-    const index = Number(extractInt(single(args)));
-    if (index >= value.size) throw new TypeError(`index out of bounds`);
-    return value.get(index);
-  },
-
   concatenate(value, args) {
     assertNoArgs(args);
     return seq(extractSeqItems(value).flatMap(extractSeqItems));
