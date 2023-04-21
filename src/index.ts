e85665c0eecda3762c784d7f6a509d7ec7103009
Thomas Smith <thom@thomthesmith.com>
2023-04-21 12:31:26 -0400

    Add load functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { parseSingleDocument, parseStream, type ParseOptions } from './parser';
 import { compose, type ComposeOptions } from './composer';
 
+import { serialize } from './serializer';
+import { present } from './presenter';
+
+import type { RepresentationNode } from './nodes';
+
 export interface LoadOptions extends ParseOptions, ComposeOptions {}
 
-export function *loadStream(text: string, options: LoadOptions = {}) {
+export function *loadStream(text: string, options: LoadOptions = {}): Generator<RepresentationNode> {
   for (const serialization of parseStream(text, options)) {
     yield compose(serialization, options);
   }
 }
 
-export function loadSingleDocument(text: string, options: LoadOptions = {}) {
+export function loadSingleDocument(text: string, options: LoadOptions = {}): RepresentationNode {
   return compose(parseSingleDocument(text, options), options);
+}
+
+export function dumpDocument(document: RepresentationNode): string {
+  return present(serialize(document));
 }
