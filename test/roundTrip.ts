5af0a5045926723e5f8f1277fdbe28b586a2b61a
Thomas Smith <thom@thomthesmith.com>
2023-04-24 12:53:08 -0400

    Use load/dump for roundtrip test

--- test/roundTrip.ts
+++ test/roundTrip.ts
@@ -7,31 +7,20 @@ if (testName === undefined) {
   process.exit(1);
 }
 
-import { prettyPrint } from './prettyPrint';
 import { Logger } from './logger';
 const logger = new Logger(process.stdout);
 
 const inputText = loadTestCase(testName);
 
 logger.log('Input text:');
-logger.indented(() => logger.logCode(inputText));
+logger.logCode(inputText);
 
 /////////
 
-import { serialize } from '@/serializer';
-import { parseSingleDocument } from '@/parser';
+import { loadSingleDocument, dumpDocument } from '@/index';
 
-const serializationTree = parseSingleDocument(inputText, { version: '1.3' });
-
-//////////
-
-import { compose } from '@/composer';
-
-const representationGraph = compose(serializationTree);
-
-//////////
-
-const serialized = serialize(representationGraph);
+const representationGraph = loadSingleDocument(inputText, { version: '1.3' });
+const presented = dumpDocument(representationGraph);
 
 logger.log('\nPresented:');
-prettyPrint(logger.write.bind(logger), serialized);
+logger.logCode(presented);
