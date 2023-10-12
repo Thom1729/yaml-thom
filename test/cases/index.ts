e171c52baef8e94f8793094fb6318f5513d2f335
Thomas Smith <thom@thomthesmith.com>
2023-10-12 19:11:06 -0400

    Remove old test files

--- test/cases/index.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-import * as fs from 'fs';
-
-export function loadTestCase(testName: string) {
-  const inputPath = path.join(__dirname, `${testName}.yaml`);
-  const inputText = fs.readFileSync(inputPath, { encoding: 'utf-8' });
-
-  return inputText;
-}
--- test/index.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import fs from 'fs';
-
-export function loadText(...pathComponents: string[]) {
-  const inputPath = path.join(__dirname, ...pathComponents);
-  return fs.readFileSync(inputPath, { encoding: 'utf-8' });
-}
