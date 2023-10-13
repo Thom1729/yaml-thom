c0d5e232d40ac390769a22bbbaa3d2ee1a868582
Thomas Smith <thom@thomthesmith.com>
2023-10-13 11:44:56 -0400

    Flesh out validation tests

--- bin/runValidationTests/index.ts
+++ bin/runValidationTests/index.ts
@@ -7,6 +7,7 @@ import {
   extractStringMap,
   defaultConstructor,
   validate, type Validator,
+  type RepresentationNode,
 } from '../lib';
 
 const BASE_TEST_PATH = path.join(
@@ -14,19 +15,46 @@ const BASE_TEST_PATH = path.join(
   '../../test/validation',
 );
 
+interface ValidationTest {
+  validator: Validator;
+  input: RepresentationNode
+}
+
+interface ValidationTestResult {
+  valid: boolean;
+}
+
+function loadValidationTest(document: RepresentationNode): ValidationTest {
+  const { validator: rawValidator, input } = extractStringMap(document, ['validator', 'input']);
+
+  const validator = defaultConstructor(rawValidator) as Validator;
+
+  return {
+    validator,
+    input,
+  };
+}
+
+function runValidationTest(test: ValidationTest): ValidationTestResult {
+  const valid = validate(test.validator, test.input);
+  return { valid };
+}
+
 export function runValidationTests(testNames: string[]) {
+  if (testNames.length === 0) {
+    testNames = fs.readdirSync(BASE_TEST_PATH);
+  }
+
   for (const testName of testNames) {
-    const filePath = path.join(BASE_TEST_PATH, testName + '.yaml');
+    const filePath = path.join(BASE_TEST_PATH, testName.endsWith('.yaml') ? testName : testName + '.yaml');
     const text = fs.readFileSync(filePath, { encoding: 'utf-8' });
 
     const test = loadSingleDocument(text);
 
-    const { validator: rawValidator, input } = extractStringMap(test, ['validator', 'input']);
+    const validationTest = loadValidationTest(test);
 
-    const validator = defaultConstructor(rawValidator);
+    const result = runValidationTest(validationTest);
 
-    const valid = validate(validator as Validator, input);
-
-    console.log(valid ? 'success' : 'failure');
+    console.log(result);
   }
 }
