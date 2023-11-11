d276fd94b1c120cdd60e287010667c8e9466a249
Thomas Smith <thom@thomthesmith.com>
2023-11-11 14:08:13 -0500

    Move load/dump into own file

---
+++ src/loadDump.ts
@@ -0,0 +1,31 @@
+import { parseSingleDocument, parseStream, type ParseOptions } from './parser';
+import { compose, type ComposeOptions } from './composer';
+
+import { SerializeOptions, serialize } from './serializer';
+import { PresentOptions, present } from './presenter';
+
+import type { RepresentationNode } from './nodes';
+
+export interface LoadOptions extends ParseOptions, ComposeOptions {}
+
+export function *loadStream(text: string, options: LoadOptions = {}): Generator<RepresentationNode> {
+  for (const serialization of parseStream(text, options)) {
+    yield compose(serialization, options);
+  }
+}
+
+export function loadSingleDocument(text: string, options: LoadOptions = {}): RepresentationNode {
+  return compose(parseSingleDocument(text, options), options);
+}
+
+export interface DumpOptions extends SerializeOptions, PresentOptions {}
+
+export function dumpDocument(document: RepresentationNode, options: DumpOptions = {}): string {
+  return present(serialize(document, options), options);
+}
+
+export function dumpStream(documents: Iterable<RepresentationNode>, options: DumpOptions = {}): string {
+  return Array.from(documents)
+    .map(document => present(serialize(document, options), options))
+    .join('');
+}
