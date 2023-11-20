0cd32a692b3c34a5ae778a3edd1d48e2d799e475
Thomas Smith <thom@thomthesmith.com>
2023-11-20 15:18:14 -0500

    Remove separate normalized validation type

--- src/validator/types.ts
+++ src/validator/types.ts
@@ -22,54 +22,32 @@ export interface Validator {
 }
 
 type ExtractOptionalArray<TBase, TChild extends (readonly TBase[] | undefined)> =
-| (Exclude<TChild, undefined> extends readonly (infer U extends TBase)[] ? U : never)
-| (undefined extends TChild ? TBase : never);
+  TChild extends readonly (infer U extends TBase)[] ? U : TBase;
 
-type ValidatorTypes<T extends Validator> = {
-  kind: ExtractOptionalArray<NodeKind, T['kind']>,
-  tag: ExtractOptionalArray<string, T['tag']>,
-
-  const: ExtractOptionalArray<unknown, T['enum']>,
-
-  items: (
-    T['items'] extends Validator
-      ? ValidatorTypes<T['items']>
-      : undefined
-  ),
-
-  properties: (
-    T['properties'] extends NodeMap<infer PairType extends readonly [RepresentationNode, Validator]>
-      ? Foo<PairType>
-      : readonly [RepresentationNode, RepresentationNode]
-  ),
-
-  anyOf: T['anyOf'],
-};
-
-type Foo<T extends readonly [RepresentationNode, Validator]> =
-  T extends readonly [RepresentationNode, Validator]
-    ? readonly [T[0], Validated<T[1]>]
-    : never
-;
-
-export type Validated<T extends Validator> = _Validated<ValidatorTypes<T>>;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type _Validated<T extends ValidatorTypes<any>> =
-& T['const']
-& {
-  'scalar': RepresentationScalar<T['tag']>,
-  'sequence': RepresentationSequence<T['tag'],
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    T['items'] extends ValidatorTypes<any>
-      ? _Validated<T['items']>
-      : RepresentationNode
-  >,
-  'mapping': RepresentationMapping<T['tag'],
-    T['properties']
-  >,
-}[T['kind']]
+export type Validated<T extends Validator> =
+& ExtractOptionalArray<unknown, T['enum']>
 & (T['anyOf'] extends readonly (infer U extends Validator)[]
   ? Validated<U>
   : unknown
-);
+)
+
+& {
+  'scalar': RepresentationScalar<ExtractOptionalArray<string, T['tag']>, string>,
+  'sequence': RepresentationSequence<ExtractOptionalArray<string, T['tag']>,
+    T['items'] extends Validator
+      ? Validated<T['items']>
+      : RepresentationNode
+  >,
+  'mapping': RepresentationMapping<ExtractOptionalArray<string, T['tag']>,
+    ValidatedMappingPairs<T['properties']>
+  >,
+}[ExtractOptionalArray<NodeKind, T['kind']>];
+
+type ValidatedMappingPairs<TProperties extends Validator['properties']> =
+  TProperties extends NodeMap<infer PairType>
+    ? (
+      PairType extends readonly [infer K extends RepresentationNode, infer V extends Validator]
+        ? readonly [K, Validated<V>]
+        : never
+    )
+    : readonly [RepresentationNode, RepresentationNode];
