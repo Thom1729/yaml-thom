4e7456616981af8883ea357202a472a5d156c0b9
Thomas Smith <thom@thomthesmith.com>
2023-11-21 12:34:42 -0500

    Refactor presenter

--- src/presenter/index.ts
+++ src/presenter/index.ts
@@ -5,7 +5,9 @@ import {
   type SerializationSequence,
   type SerializationMapping,
   type SerializationTag,
+
   NonSpecificTag,
+  ScalarStyle,
 } from '@/nodes';
 
 import { repeat, regexp } from '@/util';
@@ -13,218 +15,219 @@ import { repeat, regexp } from '@/util';
 export interface PresentOptions {
   indentation?: number;
   flow?: boolean;
+  scalarStyle?: ScalarStyle,
 }
 
 const DEFAULT_PRESENT_OPTIONS = {
   indentation: 2,
   flow: false,
-} satisfies PresentOptions;
+  scalarStyle: ScalarStyle.double,
+} satisfies Required<PresentOptions>;
 
 export function present(document: SerializationNode, options: PresentOptions = {}) {
-  const operation = new PresentOperation(options);
-  operation.presentDocument(document);
-  return operation.result.join('');
+  const computedOptions = {
+    ...DEFAULT_PRESENT_OPTIONS,
+    ...options,
+  };
+  const operation = new PresentOperation(computedOptions);
+  return Array.from(operation.presentDocument(document)).join('');
 }
 
 class PresentOperation {
-  indentation: number;
-  flow: boolean;
+  readonly options: Required<PresentOptions>;
+  private needSpace = false;
 
-  result: string[] = [];
-  needSpace = false;
-
-  constructor(options: PresentOptions) {
-    this.indentation = options.indentation ?? DEFAULT_PRESENT_OPTIONS.indentation;
-    this.flow = options.flow ?? DEFAULT_PRESENT_OPTIONS.flow;
+  constructor(options: Required<PresentOptions>) {
+    this.options = options;
   }
 
-  emit(s: string) {
+  *emit(s: string) {
     if (s.length > 0) {
-      this.result.push(s);
+      yield s;
       const last = s[s.length - 1];
       this.needSpace = last !== '\n' && last !== ' ';
     }
   }
 
-  _space() {
+  *_space() {
     if (this.needSpace) {
-      this.emit(' ');
+      yield* this.emit(' ');
     }
   }
 
-  indent(level: number) {
-    this.emit(repeat(level, ' '));
+  *indent(level: number) {
+    yield* this.emit(repeat(level, ' '));
   }
 
   implicitKey(node: SerializationNode) {
     return node.kind === 'scalar';
   }
 
-  presentDocument(node: SerializationNode) {
-    this.emit('%YAML 1.2\n');
-    this.emit('---');
-    this.presentNode(node, 0, this.flow);
-    this.emit('\n...\n');
+  *presentDocument(node: SerializationNode) {
+    yield* this.emit('%YAML 1.2\n');
+    yield* this.emit('---');
+    yield* this.presentNode(node, 0, this.options.flow);
+    yield* this.emit('\n...\n');
   }
 
-  presentNode(node: SerializationNode, level: number, flow: boolean) {
+  *presentNode(node: SerializationNode, level: number, flow: boolean) {
     if (node.kind === 'alias') {
-      this.presentAlias(node);
+      yield* this.presentAlias(node);
     } else if (node.kind === 'scalar') {
-      this.presentScalar(node);
+      yield* this.presentScalar(node);
     } else if (node.kind === 'sequence') {
       if (flow || node.size === 0) {
-        this.presentFlowSequence(node, level);
+        yield* this.presentFlowSequence(node, level);
       } else {
-        this.presentBlockSequence(node, level);
+        yield* this.presentBlockSequence(node, level);
       }
     } else if (node.kind === 'mapping') {
       if (flow || node.size === 0) {
-        this.presentFlowMapping(node, level);
+        yield* this.presentFlowMapping(node, level);
       } else {
-        this.presentBlockMapping(node, level);
+        yield* this.presentBlockMapping(node, level);
       }
     }
   }
 
-  presentAlias(node: Alias) {
-    this._space();
-    this.emit('*' + node.alias);
+  *presentAlias(node: Alias) {
+    yield* this._space();
+    yield* this.emit('*' + node.alias);
   }
 
-  presentAnchor(anchor: string | null) {
+  *presentAnchor(anchor: string | null) {
     if (anchor !== null) {
-      this._space();
-      this.emit('&' + anchor);
+      yield* this._space();
+      yield* this.emit('&' + anchor);
     }
   }
 
-  presentTag(tag: SerializationTag) {
+  *presentTag(tag: SerializationTag) {
     if (tag === NonSpecificTag.question) {
       // pass
     } else if (tag === NonSpecificTag.exclamation) {
-      this._space();
-      this.emit('!');
+      yield* this._space();
+      yield* this.emit('!');
     } else {
-      this._space();
-      this.emit('!<' + tag + '>');
+      yield* this._space();
+      yield* this.emit('!<' + tag + '>');
     }
   }
 
-  presentScalar(node: SerializationScalar) {
-    this.presentAnchor(node.anchor);
-    this.presentTag(node.tag);
+  *presentScalar(node: SerializationScalar) {
+    yield* this.presentAnchor(node.anchor);
+    yield* this.presentTag(node.tag);
 
-    this._space();
+    yield* this._space();
     if (node.tag === NonSpecificTag.question) {
-      this.presentPlainScalar(node);
+      yield* this.presentPlainScalar(node);
     } else {
-      this.presentDoubleQuotedScalar(node);
+      yield* this.presentDoubleQuotedScalar(node);
     }
   }
 
-  presentPlainScalar(node: SerializationScalar) {
+  *presentPlainScalar(node: SerializationScalar) {
     if (!canBePlainScalar(node.content)) throw new TypeError(`Cannot present ${JSON.stringify(node.content)} as plain scalar`);
-    this.emit(node.content);
+    yield* this.emit(node.content);
   }
 
-  presentDoubleQuotedScalar(node: SerializationScalar) {
-    this.emit(JSON.stringify(node.content));
+  *presentDoubleQuotedScalar(node: SerializationScalar) {
+    yield* this.emit(JSON.stringify(node.content));
   }
 
-  presentBlockSequence(node: SerializationSequence, level: number) {
-    this.presentAnchor(node.anchor);
-    this.presentTag(node.tag);
+  *presentBlockSequence(node: SerializationSequence, level: number): Generator<string> {
+    yield* this.presentAnchor(node.anchor);
+    yield* this.presentTag(node.tag);
 
     for (const item of node.content) {
-      this.emit('\n');
-      this.indent(level);
-      this.emit('-');
-      this.presentNode(item, level + this.indentation, false);
+      yield* this.emit('\n');
+      yield* this.indent(level);
+      yield* this.emit('-');
+      yield* this.presentNode(item, level + this.options.indentation, false);
     }
   }
 
-  presentBlockMapping(node: SerializationMapping, level: number) {
+  *presentBlockMapping(node: SerializationMapping, level: number): Generator<string> {
     this.presentAnchor(node.anchor);
-    this.presentTag(node.tag);
+    yield* this.presentTag(node.tag);
 
     for (const [key, value] of node.content) {
-      this.emit('\n');
-      this.indent(level);
+      yield* this.emit('\n');
+      yield* this.indent(level);
 
       if (this.implicitKey(key)) {
-        this.presentNode(key, level, true);
-        this.emit(':');
-        this.presentNode(value, level + this.indentation, false);
+        yield* this.presentNode(key, level, true);
+        yield* this.emit(':');
+        yield* this.presentNode(value, level + this.options.indentation, false);
       } else {
-        this.emit('?');
-        this.presentNode(key, level + this.indentation, false);
+        yield* this.emit('?');
+        yield* this.presentNode(key, level + this.options.indentation, false);
 
-        this.emit('\n');
-        this.indent(level);
-        this.emit(':');
-        this.presentNode(value, level + this.indentation, false);
+        yield* this.emit('\n');
+        yield* this.indent(level);
+        yield* this.emit(':');
+        yield* this.presentNode(value, level + this.options.indentation, false);
       }
     }
   }
 
-  presentFlowSequence(node: SerializationSequence, level: number) {
-    this.presentAnchor(node.anchor);
-    this.presentTag(node.tag);
+  *presentFlowSequence(node: SerializationSequence, level: number): Generator<string> {
+    yield* this.presentAnchor(node.anchor);
+    yield* this.presentTag(node.tag);
 
-    this._space();
+    yield* this._space();
     if (node.content.length === 0) {
-      this.emit('[]');
+      yield* this.emit('[]');
     } else {
-      this.emit('[');
+      yield* this.emit('[');
 
       for (const item of node.content) {
-        this.emit('\n');
-        this.indent(level + this.indentation);
-        this.presentNode(item, level + this.indentation, true);
-        this.emit(',');
+        yield* this.emit('\n');
+        yield* this.indent(level + this.options.indentation);
+        yield* this.presentNode(item, level + this.options.indentation, true);
+        yield* this.emit(',');
       }
 
-      this.emit('\n');
-      this.indent(level);
-      this.emit(']');
+      yield* this.emit('\n');
+      yield* this.indent(level);
+      yield* this.emit(']');
     }
   }
 
-  presentFlowMapping(node: SerializationMapping, level: number) {
-    this.presentAnchor(node.anchor);
-    this.presentTag(node.tag);
+  *presentFlowMapping(node: SerializationMapping, level: number): Generator<string> {
+    yield* this.presentAnchor(node.anchor);
+    yield* this.presentTag(node.tag);
     
-    this._space();
+    yield* this._space();
     if (node.content.length === 0) {
-      this.emit('{}');
+      yield* this.emit('{}');
     } else {
-      this.emit('{');
+      yield* this.emit('{');
 
       for (const [key, value] of node.content) {
         if (this.implicitKey(key)) {
-          this.emit('\n');
-          this.indent(level + this.indentation);
-          this.presentNode(key, level + this.indentation, true);
+          yield* this.emit('\n');
+          yield* this.indent(level + this.options.indentation);
+          yield* this.presentNode(key, level + this.options.indentation, true);
 
-          this.emit(':');
-          this.presentNode(value, level + this.indentation, true);
+          yield* this.emit(':');
+          yield* this.presentNode(value, level + this.options.indentation, true);
         } else {
-          this.emit('\n');
-          this.indent(level + this.indentation);
-          this.emit('?');
-          this.presentNode(key, level + this.indentation, true);
+          yield* this.emit('\n');
+          yield* this.indent(level + this.options.indentation);
+          yield* this.emit('?');
+          yield* this.presentNode(key, level + this.options.indentation, true);
 
-          this.emit('\n');
-          this.indent(level + this.indentation);
-          this.emit(':');
-          this.presentNode(value, level + this.indentation, true);
+          yield* this.emit('\n');
+          yield* this.indent(level + this.options.indentation);
+          yield* this.emit(':');
+          yield* this.presentNode(value, level + this.options.indentation, true);
         }
       }
 
-      this.emit('\n');
-      this.indent(level);
-      this.emit('}');
+      yield* this.emit('\n');
+      yield* this.indent(level);
+      yield* this.emit('}');
     }
   }
 }
