b9e2fdb59923d3b402e8334b1da0d79b684a09bb
Thomas Smith <thom@thomthesmith.com>
2023-11-21 13:26:20 -0500

    Improve presenter style handling

--- src/presenter/index.ts
+++ src/presenter/index.ts
@@ -15,13 +15,13 @@ import { repeat, regexp } from '@/util';
 export interface PresentOptions {
   indentation?: number;
   flow?: boolean;
-  scalarStyle?: ScalarStyle,
+  scalarStyle?: ScalarStyle[],
 }
 
 const DEFAULT_PRESENT_OPTIONS = {
   indentation: 2,
   flow: false,
-  scalarStyle: ScalarStyle.double,
+  scalarStyle: [ScalarStyle.double, ScalarStyle.plain],
 } satisfies Required<PresentOptions>;
 
 export function present(document: SerializationNode, options: PresentOptions = {}) {
@@ -31,6 +31,25 @@ export function present(document: SerializationNode, options: PresentOptions = {
   };
   const operation = new PresentOperation(computedOptions);
   return Array.from(operation.presentDocument(document)).join('');
+}
+
+const SCALAR_STYLE_PREDICATES = {
+  [ScalarStyle.plain]: (node: SerializationScalar) => canBePlainScalar(node.content),
+  [ScalarStyle.single]: (node: SerializationScalar) => false,
+  [ScalarStyle.double]: (node: SerializationScalar) => node.tag !== NonSpecificTag.question,
+  [ScalarStyle.block]: (node: SerializationScalar) => false,
+  [ScalarStyle.folded]: (node: SerializationScalar) => false,
+};
+
+function filter<T extends PropertyKey, U extends SerializationNode>(
+  values: Iterable<T>,
+  node: U,
+  predicates: Record<T, (node: U) => boolean>,
+) {
+  for (const value of values) {
+    if (predicates[value](node)) return value;
+  }
+  return undefined;
 }
 
 class PresentOperation {
@@ -76,17 +95,9 @@ class PresentOperation {
     } else if (node.kind === 'scalar') {
       yield* this.presentScalar(node);
     } else if (node.kind === 'sequence') {
-      if (flow || node.size === 0) {
-        yield* this.presentFlowSequence(node, level);
-      } else {
-        yield* this.presentBlockSequence(node, level);
-      }
+      yield* this.presentSequence(node, level, flow);
     } else if (node.kind === 'mapping') {
-      if (flow || node.size === 0) {
-        yield* this.presentFlowMapping(node, level);
-      } else {
-        yield* this.presentBlockMapping(node, level);
-      }
+      yield* this.presentMapping(node, level, flow);
     }
   }
 
@@ -119,10 +130,17 @@ class PresentOperation {
     yield* this.presentTag(node.tag);
 
     yield* this._space();
-    if (node.tag === NonSpecificTag.question) {
-      yield* this.presentPlainScalar(node);
-    } else {
-      yield* this.presentDoubleQuotedScalar(node);
+
+    const style = filter(this.options.scalarStyle, node, SCALAR_STYLE_PREDICATES);
+
+    if (style === undefined) throw new Error(`no valid scalar style`);
+
+    switch (style) {
+      case ScalarStyle.plain : return yield* this.presentPlainScalar(node);
+      case ScalarStyle.single: throw new Error(`Style ${style} not yet implemented`);
+      case ScalarStyle.double: return yield* this.presentDoubleQuotedScalar(node);
+      case ScalarStyle.block : throw new Error(`Style ${style} not yet implemented`);
+      case ScalarStyle.folded: throw new Error(`Style ${style} not yet implemented`);
     }
   }
 
@@ -133,6 +151,14 @@ class PresentOperation {
 
   *presentDoubleQuotedScalar(node: SerializationScalar) {
     yield* this.emit(JSON.stringify(node.content));
+  }
+
+  *presentSequence(node: SerializationSequence, level: number, flow: boolean): Generator<string> {
+    if (flow || node.size === 0) {
+      yield* this.presentFlowSequence(node, level);
+    } else {
+      yield* this.presentBlockSequence(node, level);
+    }
   }
 
   *presentBlockSequence(node: SerializationSequence, level: number): Generator<string> {
@@ -191,6 +217,14 @@ class PresentOperation {
       yield* this.emit('\n');
       yield* this.indent(level);
       yield* this.emit(']');
+    }
+  }
+
+  *presentMapping(node: SerializationMapping, level: number, flow: boolean): Generator<string> {
+    if (flow || node.size === 0) {
+      yield* this.presentFlowMapping(node, level);
+    } else {
+      yield* this.presentBlockMapping(node, level);
     }
   }
 
