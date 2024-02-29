c6104272c1c4fcf2080e9ecd0bda75da9aab6ef6
Thomas Smith <thom@thomthesmith.com>
2024-02-29 12:23:50 -0500

    Minor type improvement

--- src/util/strict.ts
+++ src/util/strict.ts
@@ -1,7 +1,6 @@
-export type BaseKey = string | number | symbol;
-type BaseEntries = Iterable<readonly [BaseKey, unknown]>;
+type BaseEntries = Iterable<readonly [PropertyKey, unknown]>;
 type FromEntries<T extends BaseEntries> = T extends Iterable<infer Pair>
-  ? [Pair] extends [readonly [infer Key extends BaseKey, unknown]]
+  ? [Pair] extends [readonly [infer Key extends PropertyKey, unknown]]
     ? { [K in Key]: (Pair & [K, unknown])[1] }
     : never
   : never;
@@ -10,7 +9,7 @@ export const strictFromEntries = Object.fromEntries as
   <T extends BaseEntries>(entries: T) => FromEntries<T>;
 
 export const objectHasOwn = Object.hasOwn as
-  <T extends object, K extends BaseKey>(o: T, v: K) => v is K & keyof T;
+  <T extends object, K extends PropertyKey>(o: T, v: K) => v is K & keyof T;
 
 export const strictKeys = Object.keys as
   // <T extends object>(o: T) => Array<keyof T & string | `${keyof T & number}`>;
