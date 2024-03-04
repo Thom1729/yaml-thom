de59e6f82926363d3f60f8074f8b7f40fb91b702
Thomas Smith <thom@thomthesmith.com>
2024-03-04 16:23:14 -0500

    Split out Evaluator

--- src/evaluator/index.ts
+++ src/evaluator/index.ts
@@ -1,16 +1,10 @@
 import {
   RepresentationNode,
   RepresentationMapping,
-  RepresentationSequence,
-  NodeComparator,
-  NodeMap,
 } from '@/nodes';
 
-import { validateAnnotation, constructAnnotation } from './annotation';
-
-import { isAnnotation } from '@/helpers';
-
-import { NestedMap } from '@/util';
+import { Evaluator } from './evaluate';
+export { EvaluationError, Evaluator } from './evaluate';
 
 import STDLIB from './stdlib';
 
@@ -29,113 +23,21 @@ export type AnnotationFunction = (
 
 export type Library = Partial<Record<string, AnnotationFunction>>;
 
-export class EvaluationError extends Error {
-  node: RepresentationNode;
-  annotation: Annotation | null;
-
-  constructor(node: RepresentationNode, annotation: Annotation | null, msg: string, cause?: unknown) {
-    super(msg, { cause });
-    Object.setPrototypeOf(this, EvaluationError.prototype);
-    this.node = node;
-    this.annotation = annotation;
-  }
+export interface EvaluationOptions {
+  annotationFunctions: Library;
 }
 
-interface EvaluationOptions {
-  annotationFunctions?: Library;
-}
-
-const DEFAULT_OPTIONS: Required<EvaluationOptions> = {
+const DEFAULT_OPTIONS: EvaluationOptions = {
   annotationFunctions: STDLIB,
 };
 
 export function evaluate(
   node: RepresentationNode,
   context: RepresentationMapping,
-  options: EvaluationOptions = {},
+  options: Partial<EvaluationOptions> = {},
 ) {
   return new Evaluator({
     ...options,
     ...DEFAULT_OPTIONS,
   }).evaluate(node, context);
-}
-
-export class Evaluator {
-  readonly cache = new NestedMap<[RepresentationNode, RepresentationMapping], RepresentationNode | null>(
-    () => new WeakMap(),
-    () => new NodeMap(),
-  );
-  readonly comparator = new NodeComparator();
-  readonly options: Required<EvaluationOptions>;
-
-  constructor(options: Required<EvaluationOptions>) {
-    this.options = options;
-  }
-
-  evaluate(
-    node: RepresentationNode,
-    context: RepresentationMapping,
-  ): RepresentationNode {
-    const cached = this.cache.get(node, context);
-    if (cached === null) {
-      throw new Error(`Recursively evaluating same annotation node with different context`);
-    } else if (cached !== undefined) {
-      return cached;
-    }
-
-    if (isAnnotation(node)) {
-      try {
-        validateAnnotation(node);
-      } catch (e) {
-        throw new EvaluationError(node, null, 'Invalid annotation node', e);
-      }
-      const annotation = constructAnnotation(node);
-
-      const annotationFunction = this.options.annotationFunctions[annotation.name];
-      if (annotationFunction === undefined) {
-        throw new EvaluationError(node, annotation, `Unknown annotation ${annotation.name}`);
-      }
-
-      try {
-        this.cache.set(node, context, null);
-        const result = annotationFunction.call(this, annotation.value, annotation.arguments, context);
-        this.cache.set(node, context, result);
-        return result;
-      } catch (e) {
-        if (e instanceof EvaluationError) {
-          throw e;
-        } else {
-          throw new EvaluationError(node, annotation, `Error evaluating annotation ${annotation.name}`, e);
-        }
-      }
-    }
-
-    switch (node.kind) {
-      case 'scalar': {
-        const result = node.clone();
-        this.cache.set(node, context, result);
-        return result;
-      }
-      case 'sequence': {
-        const result = new RepresentationSequence<string, RepresentationNode>(node.tag, []);
-        this.cache.set(node, context, result);
-        for (const item of node) {
-          result.content.push(this.evaluate(item, context));
-        }
-        return result;
-      }
-      case 'mapping': {
-        const result = new RepresentationMapping<string, readonly [RepresentationNode, RepresentationNode]>(node.tag, []);
-        this.cache.set(node, context, result);
-        for (const [key, value] of node) {
-          result.content.pairs.push([
-            this.evaluate(key, context),
-            this.evaluate(value, context),
-          ]);
-        }
-        result.content.pairs.sort((a, b) => this.comparator.compare(a[0], b[0]));
-        return result;
-      }
-    }
-  }
 }
